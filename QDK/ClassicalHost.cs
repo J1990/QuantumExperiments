8c101922ee7f8165f0691132f2f69c73fcf3f092
Jayprakash Asolkar <jayasolkar@gmail.com>
2020-08-13 19:38:09 +0100

    55% accuracy with change of layer after 6 days of execution

--- QDK/ClassicalHost.cs
+++ QDK/ClassicalHost.cs
@@ -7,6 +7,7 @@ using System.Text.Json;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using System.Diagnostics;
+using System.Text;
 
 namespace TCD.MS.IS.Dissertation
 {
@@ -17,56 +18,49 @@ namespace TCD.MS.IS.Dissertation
             try
             {
                 string json_data_path = "Data\\MNIST\\mnist_pca_30Components_3_6.json";
-                string logPath = "DebugOutput\\log.txt";
 
                 Stopwatch stopwatch = new Stopwatch();
 
                 stopwatch.Start();
 
-                using(StreamWriter writer = File.AppendText(logPath))
-                {
-                    LogWriter.WriteToLog(writer, "Time: " + DateTime.Now);
-                    LogWriter.WriteToLog(writer, "Time: " + DateTime.Now.TimeOfDay);
+                LogWriter.WriteToLog("Time: " + DateTime.Now);
+                LogWriter.WriteToLog("Time: " + DateTime.Now.TimeOfDay);
 
-                    // We start by loading the training and validation data from our JSON
-                    // data file.
-                    var data = await LoadData(json_data_path);
+                // We start by loading the training and validation data from our JSON
+                // data file.
+                var data = await LoadData(json_data_path);
 
-                    // Next, we initialize a full state-vector simulator as our target machine.
-                    using var targetMachine = new QuantumSimulator().WithTimestamps(writer);
+                // Next, we initialize a full state-vector simulator as our target machine.
+                using var targetMachine = new QuantumSimulator().WithTimestamps();
 
-                    // Once we initialized our target machine,
-                    // we can then use that target machine to train a QCC classifier.
-                    var (optimizedParameters, optimizedBias) = await TrainMnistModel.Run(
-                        targetMachine,
-                        new QArray<QArray<double>>(data.TrainingData.Features.Select(vector => new QArray<double>(vector))),
-                        new QArray<long>(data.TrainingData.Labels)
-                    );
+                // Once we initialized our target machine,
+                // we can then use that target machine to train a QCC classifier.
+                var (optimizedParameters, optimizedBias) = await TrainMnistModel.Run(
+                    targetMachine,
+                    new QArray<QArray<double>>(data.TrainingData.Features.Select(vector => new QArray<double>(vector))),
+                    new QArray<long>(data.TrainingData.Labels)
+                );
 
-                    // After training, we can use the validation data to test the accuracy
-                    // of our new classifier.
-                    var missRate = await ValidateMnistModel.Run(
-                        targetMachine,
-                        new QArray<QArray<double>>(data.ValidationData.Features.Select(vector => new QArray<double>(vector))),
-                        new QArray<long>(data.ValidationData.Labels),
-                        optimizedParameters,
-                        optimizedBias
-                    );
-                    LogWriter.WriteToLog(writer, $"Observed {100 * missRate:F2}% misclassifications.");
+                // After training, we can use the validation data to test the accuracy
+                // of our new classifier.
+                var missRate = await ValidateMnistModel.Run(
+                    targetMachine,
+                    new QArray<QArray<double>>(data.ValidationData.Features.Select(vector => new QArray<double>(vector))),
+                    new QArray<long>(data.ValidationData.Labels),
+                    optimizedParameters,
+                    optimizedBias
+                );
+                LogWriter.WriteToLog($"Observed {100 * missRate:F2}% misclassifications.");
 
-                    stopwatch.Stop();
-                    LogWriter.WriteToLog(writer, "Elapsed Total Minutes: " + stopwatch.Elapsed.TotalMinutes);
+                stopwatch.Stop();
+                LogWriter.WriteToLog("Elapsed Total Minutes: " + stopwatch.Elapsed.TotalMinutes, true);
 
-                }
-
-                
             }
             catch (Exception ex)
             {
+                LogWriter.WriteToLog("Error: " + ex.Message, true);
                 Console.WriteLine(ex.Message);
             }
-
-
         }
 
         class LabeledData
@@ -92,7 +86,7 @@ namespace TCD.MS.IS.Dissertation
 
     public static class SimulatorExtensions
     {
-        public static QuantumSimulator WithTimestamps(this QuantumSimulator sim, TextWriter textWriter)
+        public static QuantumSimulator WithTimestamps(this QuantumSimulator sim)
         {
             var stopwatch = new Stopwatch();
             stopwatch.Start();
@@ -101,7 +95,7 @@ namespace TCD.MS.IS.Dissertation
             sim.OnLog += (message) =>
             {
                 var now = stopwatch.Elapsed;
-                LogWriter.WriteToLog(textWriter, $"[{now} +{now - last}] {message}");
+                LogWriter.WriteToLog($"[{now} +{now - last}] {message}");
                 last = now;
             };
             return sim;
@@ -110,10 +104,28 @@ namespace TCD.MS.IS.Dissertation
 
     static class LogWriter
     {
-        public static void WriteToLog(TextWriter writer, string message)
+        static int count = 0;
+        static string logPath = "DebugOutput\\log.txt";
+
+        static StringBuilder stringBuilder = new StringBuilder();
+
+        public static void WriteToLog(string message, bool forceWrite = false)
         {
-            writer.WriteLine(message);
+            stringBuilder.AppendLine(message);
             Console.WriteLine(message);
+
+            count++;
+
+            if (forceWrite || count > 999)
+            {
+                using (StreamWriter writer = File.AppendText(logPath))
+                {
+                    writer.WriteLine(stringBuilder.ToString());
+                }
+
+                stringBuilder.Clear();
+                count = 0;
+            }
         }
     }
 }
